1c98a13754b559b8083148753d1b3f755b329a27
anhpt-1220 <pham.the.anh@sun-asterisk.com>
2025-04-18 14:50:17 +0700

    Fix comments

--- src/main/java/com/sun/librarymanagement/domain/repository/PublisherRepository.java
+++ src/main/java/com/sun/librarymanagement/domain/repository/PublisherRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,5 +17,5 @@ public interface PublisherRepository extends JpaRepository<PublisherEntity, Long
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT publisher FROM PublisherEntity publisher WHERE publisher.id = :id")
-    Optional<PublisherEntity> findByIdWithLock(Long id);
+    Optional<PublisherEntity> findByIdWithLock(@Param("id") Long id);
 }
